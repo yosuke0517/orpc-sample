aa125d315195699aba510c94cd8ffbafc02edb55
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2025-08-03 12:04:21 +0900

    feat: Next.jsの設定ファイルに実験的オプションを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+		cacheComponents: true,
+		reactCompiler: true,
+		// ppr: "incremental",
+		/* NOTE: PPRは cacheComponents が有効な場合、自動的に有効化される
+		 * 逆に 両方とも有効にすると競合してエラーになる
+		 */
+	},
 }
 
 export default nextConfig
